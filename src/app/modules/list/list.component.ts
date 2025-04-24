0be3dc5a2a6ad6391ff50955e1faa2764d4b1127
Plotnikova Darya <dvikhlyantseva@embedika.ru>
2025-04-24 12:44:43 +0500

    18 -> 19

--- src/app/modules/list/list.component.ts
+++ src/app/modules/list/list.component.ts
@@ -1,11 +1,12 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {ClientService} from '../../services/client.service';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: false
 })
 export class ListComponent implements OnInit {
 
